fd5af278cca1a879eab172c66741749914f8a812
leegyeongsik <leegs1012@naver.com>
2025-11-16 23:48:36 +0900

    feat : 등록할때 method수정

--- src/main/java/shop/chaekmate/common/log/registry/RegistrySendRequest.java
+++ src/main/java/shop/chaekmate/common/log/registry/RegistrySendRequest.java
@@ -3,11 +3,12 @@ package shop.chaekmate.common.log.registry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "registryClient", url = "http://localhost:12345")
 public interface RegistrySendRequest {
-    @GetMapping("/api/registry")
+    @PostMapping("/api/registry")
     void registry(@RequestParam String id,@RequestParam String host);
     @DeleteMapping("/api/unregistry")
     void unRegistry(@RequestParam String id);

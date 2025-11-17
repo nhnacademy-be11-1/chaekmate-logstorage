664dbffde670782b22f5837d5e97eb042b745f64
leegyeongsik <leegs1012@naver.com>
2025-11-17 18:01:30 +0900

    feat : threadpool에 eventtype 스레드가 들고있을 수 있게끔 LogThreadPoolExecutor 생성

---
+++ src/main/java/shop/chaekmate/common/log/executor/LogThreadPoolExecutor.java
@@ -0,0 +1,23 @@
+package shop.chaekmate.common.log.executor;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import shop.chaekmate.common.log.logging.LogContext;
+
+public class LogThreadPoolExecutor extends ThreadPoolExecutor {
+    String eventType;
+    public LogThreadPoolExecutor(int core, int max, long keepAlive, TimeUnit unit, BlockingQueue<Runnable> queue, String eventType) {
+        super(core, max, keepAlive, unit, queue);
+        this.eventType = eventType;
+    }
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        LogContext.setThreadLocal(eventType);
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        LogContext.clearThreadLocal();
+    }
+}

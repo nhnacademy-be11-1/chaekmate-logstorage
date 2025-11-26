587b08df507874e14b1b7c6f9f34da43304cac42
leegyeongsik <leegs1012@naver.com>
2025-11-26 10:01:40 +0900

    fix : Log 파라미터 serviceName 제거

--- src/main/java/shop/chaekmate/common/log/logging/Log.java
+++ src/main/java/shop/chaekmate/common/log/logging/Log.java
@@ -5,102 +5,107 @@ import shop.chaekmate.common.log.dto.ErrorLog;
 import shop.chaekmate.common.log.dto.InfoLog;
 import shop.chaekmate.common.log.dto.ResponseTimeLog;
 import shop.chaekmate.common.log.worker.LogWorker;
+
 public class Log {
     private static final String UNKNOWN_EVENT_TYPE = "UNKNOWN";
 
-    public static void InfoWithEventType(String serviceName,
-                                         String eventType,
-                                         String message,
-                                         Object... args) {
+    public static void InfoWithEventType(
+            String eventType,
+            String message,
+            Object... args) {
         StackTraceElement caller = CallerResolver.resolveCaller();
-        submit(convertInfoLog(serviceName, eventType, caller.getClassName(), caller.getMethodName(), message, args));
+        submit(convertInfoLog(eventType, caller.getClassName(), caller.getMethodName(), message, args));
     }
 
-    public static void Info(String serviceName,
-                            String message,
-                            Object... args) {
+    public static void Info(
+            String message,
+            Object... args) {
         StackTraceElement caller = CallerResolver.resolveCaller();
-        submit(convertInfoLog(serviceName, LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE), caller.getClassName(),
+        submit(convertInfoLog(LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE), caller.getClassName(),
                 caller.getMethodName(), message, args));
     }
 
-    public static void InfoWithClassMethod(String serviceName,
-                                           String className,
-                                           String method,
-                                           String message,
-                                           Object... args) {
-        submit(convertInfoLog(serviceName, LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE), className, method,
+    public static void InfoWithClassMethod(
+            String className,
+            String method,
+            String message,
+            Object... args) {
+        submit(convertInfoLog(LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE), className, method,
                 message, args));
     }
 
-    protected static void InfoWithEventTypeClassMethod(String serviceName,
-                                                       String eventType,
-                                                       String className,
-                                                       String methodName,
-                                                       String message,
-                                                       Object... args) {
-        submit(convertInfoLog(serviceName, eventType, className, methodName, message, args));
+    protected static void InfoWithEventTypeClassMethod(
+            String eventType,
+            String className,
+            String methodName,
+            String message,
+            Object... args) {
+        submit(convertInfoLog(eventType, className, methodName, message, args));
     }
 
 
-    public static void ErrorWithEventType(String serviceName,
-                                          String eventType,
-                                          Exception e,
-                                          int status,
-                                          String message,
-                                          Object... args) {
-        submit(convertErrorLog(serviceName, eventType, e, status, message, args));
+    public static void ErrorWithEventType(
+            String eventType,
+            Exception e,
+            int status,
+            String message,
+            Object... args) {
+        submit(convertErrorLog(eventType, e, status, message, args));
     }
 
-    public static void Error(String serviceName,
-                             Exception e,
-                             int status,
-                             String message,
-                             Object... args) {
-        submit(convertErrorLog(serviceName, LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE), e, status, message,
+    public static void Error(
+            Exception e,
+            int status,
+            String message,
+            Object... args) {
+        submit(convertErrorLog(LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE), e, status, message,
                 args));
     }
 
-    public static void ResponseTimeWithEventType(String serviceName,
-                                            Long responseTime,
-                                            String eventType,
-                                            String message,
-                                            Object... args) {
+    public static void ResponseTimeWithEventType(
+            Long responseTime,
+            String eventType,
+            String message,
+            Object... args) {
         StackTraceElement callerResolver = CallerResolver.resolveCaller();
-        submit(convertResponseTime(serviceName,responseTime,eventType,callerResolver.getClassName(),callerResolver.getMethodName(),message,args));
+        submit(convertResponseTime(responseTime, eventType, callerResolver.getClassName(),
+                callerResolver.getMethodName(), message, args));
     }
 
 
-
-    public static void ResponseTime(String serviceName,
-                                    Long responseTime,
-                                    String message,
-                                    Object... args) {
+    public static void ResponseTime(
+            Long responseTime,
+            String message,
+            Object... args) {
         StackTraceElement callerResolver = CallerResolver.resolveCaller();
-        submit(convertResponseTime(serviceName,responseTime,LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE),callerResolver.getClassName(),callerResolver.getMethodName(),message,args));
+        submit(convertResponseTime(responseTime, LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE),
+                callerResolver.getClassName(), callerResolver.getMethodName(), message, args));
     }
 
-    public static void ResponseTimeWithClassMethod(String serviceName,
-                                                   Long responseTime,
-                                                   String className,
-                                                   String methodName,
-                                                   String message,
-                                                   Object... args){
-        submit(convertResponseTime(serviceName,responseTime,LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE),className,methodName,message,args));
+    public static void ResponseTimeWithClassMethod(
+            Long responseTime,
+            String className,
+            String methodName,
+            String message,
+            Object... args) {
+        submit(convertResponseTime(responseTime, LogContext.getEventType().orElse(UNKNOWN_EVENT_TYPE), className,
+                methodName, message, args));
     }
-    protected static void ResponseTimeWithEventTypeClassMethod(String serviceName,
-                                                   String eventType,
-                                                   Long responseTime,
-                                                   String className,
-                                                   String methodName,
-                                                   String message,
-                                                   Object... args){
-        submit(convertResponseTime(serviceName,responseTime,eventType,className,methodName,message,args));
+
+    protected static void ResponseTimeWithEventTypeClassMethod(
+            String eventType,
+            Long responseTime,
+            String className,
+            String methodName,
+            String message,
+            Object... args) {
+        submit(convertResponseTime(responseTime, eventType, className, methodName, message, args));
     }
-    private static InfoLog convertInfoLog(String serviceName, String eventType, String className, String methodName,
+
+    private static InfoLog convertInfoLog(String eventType, String className, String methodName,
                                           String message, Object[] args) {
         return InfoLog.of(
-                serviceName,
+                LogContext.serviceName,
                 eventType,
                 className,
                 methodName,
@@ -109,10 +114,10 @@ public class Log {
         );
     }
 
-    private static BaseLog convertErrorLog(String serviceName, String eventType, Exception e, int status,
+    private static BaseLog convertErrorLog(String eventType, Exception e, int status,
                                            String message, Object[] args) {
         return ErrorLog.of(
-                serviceName,
+                LogContext.serviceName,
                 eventType,
                 e,
                 status,
@@ -120,9 +125,11 @@ public class Log {
                 args
         );
     }
-    private static BaseLog convertResponseTime(String serviceName, Long responseTime, String eventType, String className, String methodName, String message, Object[] args) {
+
+    private static BaseLog convertResponseTime(Long responseTime, String eventType, String className, String methodName,
+                                               String message, Object[] args) {
         return ResponseTimeLog.of(
-                serviceName,
+                LogContext.serviceName,
                 eventType,
                 responseTime,
                 className,
@@ -130,6 +137,7 @@ public class Log {
                 message,
                 args);
     }
+
     private static void submit(BaseLog log) {
         LogContext.threadPool().submit(new LogWorker(LogContext.storage(), log));
     }
